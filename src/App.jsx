798d08caad1607072d2f8b6fc93ef63f6150b060
CaioMassola <caio.massola@outlook.com>
2022-12-28 13:20:01 -0300

    implements: exercise resolution

--- src/App.jsx
+++ src/App.jsx
@@ -15,23 +15,76 @@ import { useState } from 'react';
 // todo - Mostre um alerta caso o login seja efetuado com sucesso (javascript alert). Investigue a função login() para entender como ter sucesso na requisição.
 
 export default function LoginForm() {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setError] = useState('');
+  const [blockBtn, setBlockBtn] = useState(false);
+
+  const getLogin = () => {
+    setBlockBtn(true)
+    setError('')
+    if(email && password) {
+    login({email, password}).then((x) => {
+        alert(`Success!${'\n'}${x.message}`)
+        setBlockBtn(false)
+        setEmail('')
+        setPassword('')
+
+      }).catch((e) => {
+        setError(e.message)
+        setBlockBtn(false)
+      }).finally(() => {
+        setBlockBtn(false)
+      })
+    } else {
+      setError('E-mail or password wrong.')
+      setBlockBtn(false)
+    }
+  }
+
+  const handleEmail = (x) => {
+    setEmail(x.target.value)
+    setError('')
+  }
+
+  const handlePassword = (x) => {
+    setPassword(x.target.value)
+    setError('')
+  }
   return (
     <div className='wrapper'>
       <div className='login-form'>
         <h1>Login Form 🐞</h1>
         {/* Coloque a mensagem de erro de login na div abaixo. Mostre a div somente se houver uma mensagem de erro. */}
-        <div className='errorMessage'></div>
+        {
+          errorMessage && (
+           <div className='errorMessage'>{errorMessage}</div> 
+          )
+        }
         <div className='row'>
           <label htmlFor={'email'}>Email</label>
-          <input id={'email'} type={'email'} autoComplete='off' />
+          <input 
+            id={'email'} 
+            type={'email'} 
+            autoComplete='off' 
+            value={email} placeholder={'Email...'} 
+            onChange={(x) => handleEmail(x) } 
+          />
         </div>
         <div className='row'>
           <label htmlFor={'password'}>Password</label>
-          <input id={'password'} type={'password'} />
+          <input 
+            id={'password'} 
+            type={'password'} 
+            value={password} 
+            placeholder={'Password...'} 
+            onChange={(x) => handlePassword(x)}
+          />
         </div>
 
         <div className='button'>
-          <button>Login</button>
+          <button onClick={() => getLogin()} disabled={blockBtn ? true : false}>Login</button>
         </div>
       </div>
     </div>
--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,14 @@
 export function login({ email, password }) {
-  const delay = (0.7 + Math.random() * 2) * 1000;
 
+  const delay = (0.7 + Math.random() * 2) * 1000;
   return new Promise((resolve, reject) => {
     setTimeout(function () {
-      if (password === 'password123' && !!email) {
-        resolve();
+      let emailProviderExists = email[email.indexOf('@') + 1] == '.' ? false : true
+      let errorMessage = 'E-mail or password wrong.'
+      if (password === 'password123' && email[0] != '@' && email.includes("@") && email.includes(".com") && emailProviderExists) {
+        resolve({ message: 'You have been authenticated!' });
       } else {
-        reject({ message: 'e-mail or password wrong.' });
+        reject({ message: errorMessage });
       }
     }, delay);
   });
